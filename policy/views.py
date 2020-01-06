93994fb42a512fe4d4acb0fb046297b8419fc2d1
sakura09 <33317080+sakura09@users.noreply.github.com>
2020-01-06 16:16:41 +0800

    修复适用区域问题

--- policy/views.py
+++ policy/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponse, response
 from django.http import JsonResponse
 from django.core import serializers
 from django.db.models import Q
+import re
 
 import json
 
@@ -81,33 +82,43 @@ def contrastPolicy(request):
         request.params = request.GET
 
     # 筛选条件, 传递普通数组
+    #省份数组
     provinceList = request.GET.getlist('province', [])
-    #provinceList = json.loads(provinceList)
+    #provinceList = request.GET.get('province', '')
+    provinceList = json.loads(provinceList[0])
     print(provinceList[0])
     #print(provinceList[1])
 
-    policyTypeList = request.GET.getlist('政策类别', [])
-    #policyTypeList = json.loads(policyTypeList[0])
+    #政策类型数组
+    policyTypeList = request.GET.getlist('政策类型', [])
+    policyTypeList = json.loads(policyTypeList[0])
+    #服务类型数组
     serverTypeList = request.GET.getlist('服务类型', [])
     print(serverTypeList[0])
-    #serverTypeList = json.loads(serverTypeList[0])
+    serverTypeList = json.loads(serverTypeList[0])
 
     #页码不同
-    #page_list = request.GET.get('pageNum1', [])
-    num = request.GET.get('pageNum1', default='1')
-    index = request.GET.get('pageNum2', default='1')
-    #page_list = json.loads(page_list[0])
-    print(num)
-    try:
-        index = (int(index) - 1) * 10
-    except:
-        index = (int(index) - 1) * 10
-    index2 = index + 10
-    num = (int(num) - 1) * 10
-    print("页码2")
+    pageList = request.GET.getlist('pageNum', [])
+    pageList = json.loads(pageList[0])
+    if len(pageList) == 1:
+        num = pageList[0]
+        num = (int(num) - 1) * 10
+        num2 = num + 10
+        print("只有一个页码")
+    elif len(pageList) == 2:
+        num = pageList[0]
+        index =pageList[1]
+        print(num)
+        try:
+            index = (int(index) - 1) * 10
+        except:
+            index = (int(index) - 1) * 10
+        index2 = index + 10
+        num = (int(num) - 1) * 10
+        num2 = num + 10
+        print("页码2")
 
     # num1 = int(num)
-    num2 = num+ 10
     print("num_2")
     print(num2)
 
@@ -117,6 +128,8 @@ def contrastPolicy(request):
     print(serverTypeList)
     print(num)
     print(index)
+
+
 
     #只有一个省份
     if len(provinceList) == 1:
@@ -132,6 +145,8 @@ def contrastPolicy(request):
         print(len(serverTypeList))
 
         l1 = total_method_policy.filter(province=provinceList[0])
+        print('l1.len')
+        print(len([_ for _ in l1]))
         q1 = Q()
         q1.connector = 'OR'
         if policyTypeList:
@@ -144,9 +159,15 @@ def contrastPolicy(request):
             for i in serverTypeList:
                 #    l1 = l1.filter(intent=i)
                 q1.children.append(('type', i))
+
+        print('q1.len')
+        print(len(q1))
         l1 = l1.filter(q1)
 
+
         total_num = len([_ for _ in l1])
+        print('total_num')
+        print(total_num)
         method_list = []
         type_list = []
 
@@ -163,7 +184,7 @@ def contrastPolicy(request):
             type = line.type
             data = {
                 '政策标题': title,
-                '省份': province,
+                '适用区域': province,
                 '人才待遇': method,
                 'url': url,
                 '政策条款及服务内容': content,
@@ -233,8 +254,8 @@ def contrastPolicy(request):
             type1 = line.type
             data1 = {
                 '政策标题': title,
-                '省份': province,
-                '人才待遇': method1,
+                '适用区域': province,
+                '人才待遇1': method1,
                 'url': url,
                 '政策条款及服务内容': content,
                 '待遇类型': type1
@@ -254,7 +275,7 @@ def contrastPolicy(request):
             type2 = line.type
             data2 = {
                 '政策标题': title,
-                '省份': province,
+                '适用区域': province,
                 '人才待遇': method2,
                 'url': url,
                 '政策条款及服务内容': content,
@@ -267,7 +288,7 @@ def contrastPolicy(request):
         print(len(r1))
         print("r2")
         print(len(r2))
-        result = JsonResponse({'total_num1': total_num1, 'total_num2': total_num2, 'treatList1': r1, 'treatList2': r2},
+        result = JsonResponse({'total_num1': total_num1, 'total_num2': total_num2, 'treat1': r1, 'treatList2': r2},
                               json_dumps_params={'ensure_ascii': False})
         #
         result["Access-Control-Allow-Origin"] = "http://192.168.1.12:8080"
@@ -306,4 +327,55 @@ def test(request):
     # m.method
     # print(m.type)
     # print(m.method)
+
+
+#参数是选择的政策类别
+def test2(method_type):
+    print("test2")
+    L = total_method_policy.filter(province='浙江省')
+    print(L[0].stag_id)
+    str = L[0].method
+    print(str)
+
+
+    #统计折线图的x, y轴数据
+    pattern = re.compile(r'\d+万元')
+    res = re.findall(pattern, str)
+    p = re.compile(r'\d+')
+    res2 = re.findall(p, res[0])
+    res2 = int(res2[0])
+    print(type(res2))
+    print(res2)
+
+    sum_p = [0 for _ in range(1000)]
+    sum_p[2] = 3
+    sum_p[999] = 999
+    print(len(sum_p))
+    print(sum_p[999])
+    sum_p[2] = sum_p[2]+1
+    print(sum_p[2])
+
+    l1 = total_method_policy.filter(method = method_type)
+
+    for line in l1:
+        str = line.method
+        pattern = re.compile(r'\d+万元')
+        res = re.findall(pattern, str)
+        pattern2 = re.compile(r'\d+亿元')
+        res2 = re.findall((pattern2, str))
+        # p2 = re.compile(r'\d+')
+        # res2 = re.findall(p2, res[0])
+        # res2 = int(res2[0])
+        if len(res) > 0:
+            for r in res:
+                p2 = re.compile(r'\d+')
+                r1 = re.findall(p2, r)
+                r1= int(r1[0])
+                sum_p[r1] = sum_p[r1]+1
+
+
+
+
+
+
 
